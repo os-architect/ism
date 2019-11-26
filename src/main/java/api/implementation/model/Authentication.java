0c2722d18b4323058cf8d549aa5e552c14d50056
os-architect <cnidus@yandex.com>
2019-11-26 00:22:26 -0800

    Populating Authentication as a POJO

--- src/main/java/api/implementation/model/Authentication.java
+++ src/main/java/api/implementation/model/Authentication.java
@@ -2,8 +2,32 @@ package api.implementation.model;
 
 import api.meta.model.Model;
 
+import java.util.List;
+
 public class Authentication extends Model {
 
+    private long sessionStart;
+    private List<String> policies;
 
+    public Authentication(long sessionStart, List<String> policies) {
+        this.sessionStart = sessionStart;
+        this.policies = policies;
+    }
+
+    public long getSessionStart() {
+        return sessionStart;
+    }
+
+    public void setSessionStart(long sessionStart) {
+        this.sessionStart = sessionStart;
+    }
+
+    public List<String> getPolicies() {
+        return policies;
+    }
+
+    public void setPolicies(List<String> policies) {
+        this.policies = policies;
+    }
 
 }
