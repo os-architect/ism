6737299cfc685756f910a9edfa4aeac08a03c611
os-architect <cnidus@yandex.com>
2020-01-18 14:39:10 -0800

    Better UI arrengement

--- watcher/app/components/profile.js
+++ watcher/app/components/profile.js
@@ -1,5 +1,21 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    classNames: ["profile"]
+    classNames: ["profile"],
+
+    actions: {
+
+        movenorth: function() {
+        },
+        movesouth: function() {
+        },
+        moveeast: function() {
+        },
+        movewest: function() {
+        },
+        stopmoving: function() {
+        },
+
+    }
+
 });
